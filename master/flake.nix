2fd7a5321c53fd44d8391fb75b82499bf6ad4a3b
hugosenari <hugosenari@gmail.com>
2022-03-12 04:00:49 -0300

    chore: re index sdnotify

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Systemd service notification helper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-sdnotify-master.flake = false;
+  inputs.src-sdnotify-master.owner = "FedericoCeratto";
+  inputs.src-sdnotify-master.ref   = "refs/heads/master";
+  inputs.src-sdnotify-master.repo  = "nim-sdnotify";
+  inputs.src-sdnotify-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-sdnotify-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-sdnotify-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
